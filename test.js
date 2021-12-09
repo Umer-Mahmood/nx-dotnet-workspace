457b8e1b9ac94f522a347e589c4976fa523982ef
Umer Mahmood <41982083+Umer-Mahmood@users.noreply.github.com>
2021-12-09 01:33:17 +0100

    testing is ci function

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 function main(){
     console.log("hello");
+    var resp = IsCiOnPlatform('windows');
+    console.log("response:",resp);
 }
+
+function IsCiOnPlatform(platformName) {
+    return (
+      isEnvVarSet('CI') &&
+      isEnvVarSet('RUNNER_OS') &&
+      process.env['RUNNER_OS'] == platformName
+    );
+    }
 
 main();
